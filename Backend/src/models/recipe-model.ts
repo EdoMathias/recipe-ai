f09eb2aa73ab4e7066e615696f829e974e71c2a7
Edo Mathias <edo.mathias@gmail.com>
2024-07-26 14:24:22 +0300

    Added recipe model

---
+++ Backend/src/models/recipe-model.ts
@@ -0,0 +1,33 @@
+import { eRole } from '../enums/role';
+import Joi from 'joi';
+import { ValidationError } from './client-errors';
+import { RecipeSteps } from './recipe-steps-model';
+
+export class RecipeModel {
+  public id: number;
+  public recipeName: string;
+  public recipeType: string[];
+  public recipeSteps: RecipeSteps;
+
+  constructor(recipe: RecipeModel) {
+    this.id = recipe.id;
+    this.recipeName = recipe.recipeName;
+    this.recipeType = recipe.recipeType;
+    this.recipeSteps = recipe.recipeSteps;
+  }
+
+  // validation
+  private static registerValidationSchema = Joi.object({
+    id: Joi.number().forbidden(),
+    recipeName: Joi.string().required().min(2).max(50),
+    recipeType: Joi.array().items(Joi.string().min(1)).required().min(1),
+    recipeSteps: Joi.object().pattern(Joi.string().min(1), Joi.string().min(1)),
+  });
+
+  public validateRegister(): void {
+    const result = RecipeModel.registerValidationSchema.validate(this);
+    if (result.error) {
+      throw new ValidationError(result.error.message);
+    }
+  }
+}
