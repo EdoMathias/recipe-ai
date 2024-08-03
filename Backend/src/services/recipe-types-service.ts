8f4fb9190030c4dab57bae58a376255c88cf7a8e
Edo Mathias <edo.mathias@gmail.com>
2024-08-03 10:59:52 +0300

    lnit recipe types service

--- Backend/src/services/recipe-types-service.ts
+++ Backend/src/services/recipe-types-service.ts
@@ -1,28 +1,29 @@
 import { ResourceNotFoundError } from '../models/client-errors';
-import { RecipeModel } from '../models/recipe-model';
+import { RecipeType } from '../models/recipe-types';
 import { dal } from '../utils/dal';
 import { OkPacketParams, ResultSetHeader, RowDataPacket } from 'mysql2';
 
 class RecipeTypesService {
   //----------------------------------------------------------------------------
-  public async getAllRecipes(): Promise<string[]> {
+  public async getAllRecipeTypes(): Promise<RecipeType[]> {
     const sqlQuery = 'SELECT * FROM recipe_types';
 
     const rowDataPacket: RowDataPacket[] =
       await dal.execute<RowDataPacket[]>(sqlQuery);
 
     // Convert to unknown first to avoid type error
-    const recipeTypes: string[] = rowDataPacket as unknown as string[];
+    const recipeTypes: RecipeType[] = rowDataPacket as RecipeType[];
 
     return recipeTypes;
   }
 
   //----------------------------------------------------------------------------
-  public async getRecipeTypeById(id: number): Promise<string> {
-    const sqlQuery = `SELECT * FROM recipes WHERE id = ${id}`;
+  public async getRecipeTypeById(id: number): Promise<RecipeType> {
+    console.log('id:', id);
+    const sqlQuery = `SELECT * FROM recipe_types WHERE id = ${id}`;
 
     const rowDataPacket: RowDataPacket[] = await dal.execute(sqlQuery);
-    const recipeType: string = rowDataPacket[0] as unknown as string;
+    const recipeType: RecipeType = rowDataPacket[0] as RecipeType;
 
     if (!recipeType) {
       throw new ResourceNotFoundError(id);
@@ -32,7 +33,7 @@ class RecipeTypesService {
   }
 
   //----------------------------------------------------------------------------
-  public async addRecipeType(recipeType: string): Promise<string> {
+  public async addRecipeType(recipeType: string): Promise<RecipeType> {
     const sqlQuery = 'INSERT INTO recipe_types(type_name) VALUES(?)';
 
     const values = [recipeType];
@@ -45,6 +46,24 @@ class RecipeTypesService {
     const addedRecipeType = await this.getRecipeTypeById(info.insertId);
 
     return addedRecipeType;
+  }
+
+  //----------------------------------------------------------------------------
+  public async deleteRecipeType(id: number): Promise<void> {
+    const sqlQuery = 'DELETE FROM recipe_types WHERE id = ?';
+
+    const values = [id];
+
+    const info: OkPacketParams = await dal.execute<ResultSetHeader>(
+      sqlQuery,
+      values,
+    );
+
+    if (info.affectedRows === 0) {
+      throw new ResourceNotFoundError(id);
+    }
+
+    return;
   }
 }
 
