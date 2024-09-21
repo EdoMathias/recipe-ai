ccac9b1fb7eb9ea4d9fcf5fa63fe52d1657ea656
Edo Mathias <edo.mathias@gmail.com>
2024-09-21 19:18:12 +0300

    Redesign home component to display cards

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './Home.css';
+import { Box, Grid2, Typography } from '@mui/material';
 import RecipeCard from '../../RecipeCard/RecipeCard';
 import { recipesMock } from '../../../DataMocks/RecipesMock';
 
 function Home(): React.ReactElement {
   return (
     <div className="Home">
-      <h2>Home Page...</h2>
+      <Typography variant="h2" gutterBottom>
+        Which recipe would you like to cook today?
+      </Typography>
 
-      {recipesMock.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} />
-      ))}
+      <Grid2 container spacing={2} justifyContent="center" sx={{ padding: 2 }}>
+        {recipesMock.map((recipe) => (
+          <Grid2
+            key={recipe.id}
+            sx={{
+              flexGrow: 1,
+              maxWidth: 345, // Set the fixed width for each card
+              flexBasis: '345px', // Ensure each card takes up a fixed size
+            }}
+          >
+            <Box sx={{ padding: 1 }}>
+              <RecipeCard recipe={recipe} />
+            </Box>
+          </Grid2>
+        ))}
+      </Grid2>
     </div>
   );
 }
